6b27529f3f7fba35a79e8a5f5ac5a704a5a38982
Lioncat Dev <74904820+Lioncat2002@users.noreply.github.com>
2021-07-01 07:04:18 +0000

    Added music error handles

--- Cogs/music.py
+++ Cogs/music.py
@@ -7,15 +7,9 @@ class music(commands.Cog):
   
   
 
-  def issupported(url):
-    extractors = youtube_dl.extractor.gen_extractors()
-    for e in extractors:
-        if e.suitable(url) and e.IE_NAME != 'generic':
-            return True
-    return False
 
   @commands.command()
-  async def play(self,ctx,url):
+  async def play(self,ctx,url=None):
     YTDL_OPTS = {
     'format': 'bestaudio/best',
     'restrictfilenames': True,
@@ -29,20 +23,31 @@ class music(commands.Cog):
     'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
   }
     
-    
-    voice_channel=ctx.author.voice.channel
-    if voice_channel==None:
+    if url==None:
+      embed=discord.Embed(title="Error!",description="No youtube url provided!",color=discord.Colour.red())
+      await ctx.send(embed=embed)
+      return
+    if url[:23]!='https://www.youtube.com':
+      embed=discord.Embed(title="Error!",description="Please provide a valid youtube url!",color=discord.Colour.red())
+      await ctx.send(embed=embed)
+      return
+
+    if ctx.author.voice!=None:  
+      voice_channel=ctx.author.voice.channel
+    else:
       embed=discord.Embed(title="Error!",description="Please join a voice channel to use this command!",color=discord.Colour.red())
       await ctx.send(embed=embed)
+      return
+
     
-    else:
-      print("Connecting")
-      vc=await voice_channel.connect()
-      print("Connected!")
-      ytdl = youtube_dl.YoutubeDL(YTDL_OPTS)
-      info = ytdl.extract_info(url, download=False)
-      asrc = discord.FFmpegOpusAudio(info['formats'][0]['url'], before_options="-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5")
-      vc.play(asrc)
+    
+    print("Connecting")
+    vc=await voice_channel.connect()
+    print("Connected!")
+    ytdl = youtube_dl.YoutubeDL(YTDL_OPTS)
+    info = ytdl.extract_info(url, download=False)
+    asrc = discord.FFmpegOpusAudio(info['formats'][0]['url'], before_options="-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5")
+    vc.play(asrc)
 
   @commands.command()
   async def leave(self,ctx):
